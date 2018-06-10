9e8ecc9b6447d22e26ee6f8974d11383b5f92da4
75py <dev.75py@gmail.com>
2018-06-10 16:00:12 +0900

    Refactor to AndroidX (AS3.2 Canary 17)

--- app/src/main/java/com/nagopy/android/ivyleemethod/MainActivity.kt
+++ app/src/main/java/com/nagopy/android/ivyleemethod/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.nagopy.android.ivyleemethod
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nagopy.android.ivyleemethod.ui.main.MainFragment
 
